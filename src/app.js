fda81f3d60b1d6d67d36b720a7a5071556c2921e
Lair <shadow@Liar.local>
2018-11-13 22:40:45 +0800

    修改目录结构

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import styles from './app.css'
+import styles from './assets/style/app.css'
 
 ReactDOM.render(<a style={styles.dddd}>sssss</a>, document.getElementById('root'))
